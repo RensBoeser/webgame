18462123fa9e08beff18f45b9ad53cd4393715c9
noahplazier <noahplz@hotmail.nl>
2019-10-15 21:57:47 +0200

    movement visualization, optimization

--- public/js/screen.js
+++ public/js/screen.js
@@ -42,7 +42,6 @@ socket.on("players", currentUsers => {
 			const playerArrow = element.getElementsByClassName("player__arrow")[0];
 			if (playerArrow) {
 				const rotation = Math.atan2(user.movement.y, user.movement.x) - Math.PI * 0.5;
-				// const speed = Math.max(Math.abs(user.movement.x), Math.abs(user.movement.y));
 				const speed = Math.sqrt(Math.pow(user.movement.x, 2) + Math.pow(user.movement.y, 2));
 				playerArrow.style.transform = `rotate(${rotation}rad)`;
 				playerArrow.style.opacity = speed;
